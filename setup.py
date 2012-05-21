2c3f9924b0aa1d13fa1cf3e56886ce9235e0bef0
Ruslan Spivak <ruslan.spivak@gmail.com>
2012-05-20 20:47:20 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,56 @@
+###############################################################################
+#
+# Copyright (c) 2012 Ruslan Spivak
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+###############################################################################
+
+__author__ = 'Ruslan Spivak <ruslan.spivak@gmail.com>'
+
+import os
+
+from setuptools import setup, find_packages
+
+
+classifiers = """\
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Software Development :: Libraries
+Operating System :: Unix
+"""
+
+def read(*rel_names):
+    return open(os.path.join(os.path.dirname(__file__), *rel_names)).read()
+
+
+setup(
+    name='logsna',
+    version='1.0',
+    url='https://github.com/rspivak/logsna',
+    license='MIT',
+    descriptifon='A small Python library that provides a sane log output format',
+    author='Ruslan Spivak',
+    author_email='ruslan.spivak@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=filter(None, classifiers.split('\n')),
+    long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
+    )
